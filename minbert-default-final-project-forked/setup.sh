fecdc1ae05c7bd6927b839e28a4860c03fa4b8b7
sago2693 <56797497+sago2693@users.noreply.github.com>
2023-06-08 23:41:15 +0200

    updated setup script, create custom config class in notebook only

--- minbert-default-final-project-forked/setup.sh
+++ minbert-default-final-project-forked/setup.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+
+# Create and activate the environment
+if [[ "$OSTYPE" == "msys" ]]; then
+    conda create -n dnlp python=3.8
+    conda activate dnlp
+else
+    conda create -n dnlp python=3.8
+    source activate dnlp
+fi
+
+
 conda create -n dnlp python=3.8
 conda activate dnlp
-
+conda install -c conda-forge jupyterlab
 conda install pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
 pip install tqdm==4.58.0
 pip install requests==2.25.1
